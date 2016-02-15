ac5ebe14a99813ce64a7f90d69e97dad58eb89e4
chris-tomich <chris.tomich@gmail.com>
2016-02-15 23:11:14 +0800

    Fixed up the examples to be more useful

--- ExplanationOfThisWithCats/explanationOfThisWithCats.js
+++ ExplanationOfThisWithCats/explanationOfThisWithCats.js
@@ -1,64 +1,87 @@
+/**
+ * This is a function that represents a "ball".
+ */
 function ball() {
     if (this.name) {
-        return this.name + " is playing with the ball!";
+        console.log(this.name + " is playing with the ball!");
     }
     else {
-        return "The ball just sits there.";
+        console.log("The ball just sits there.");
     }
-}
-
-function yarn() {
-    if (this.name) {
-        return this.name + " is playing with the yarn!";
-    }
-    else {
-        return "The yarn just sits there.";
-    }
-}
-
-function Cat(name) {
-    this.name = name;
 }
 
 /**
- * We haven't given anyone the ball.
+ * This is a function that represents a "ball of yarn".
  */
-console.log(ball());
+function yarn() {
+    if (this.name) {
+        console.log(this.name + " is playing with the ball of yarn!");
+    }
+    else {
+        console.log("The ball of yarn just sits there.");
+    }
+}
+
+/**
+ * This is a Cat prototype.
+ */
+function Cat(name) {
+    this.name = name;
+    this.toy = function () {
+        console.log(this.name + " doesn't have a toy to play with. :(");
+    }
+    this.play = function () {
+        this.toy();
+    }
+}
+
+/**
+ * We haven't given the ball to any of our cats.
+ */
+ball();
+
+/**
+ * We haven't given the ball of yarn to any of our cats.
+ */
+yarn();
 
 var amelia = new Cat("Amelia");
 /**
+ * We ask amelia to play.
+ */
+amelia.play();
+/**
  * We give Amelia the 'ball'.
  */
-amelia.playWithBall = ball;
-console.log(amelia.playWithBall());
+amelia.toy = ball;
+amelia.play();
 
 var frank = new Cat("Frank");
 /**
- * We give Frank the 'ball'.
+ * We give Frank the 'yarn'.
  */
-frank.playWithBall = ball;
-console.log(frank.playWithBall());
+frank.toy = yarn;
+frank.play();
 
 var josephine = new Cat("Josephine");
 /**
  * Josephine is smarter and she can play with multiple toys.
  */
-josephine.excitedToPlayWith = function (toy) {
-    var action = toy.call(this);
-    return action;
+josephine.playWith = function (toy) {
+    toy.call(this);
 }
-console.log(josephine.excitedToPlayWith(ball));
-console.log(josephine.excitedToPlayWith(yarn));
+josephine.playWith(ball);
+josephine.playWith(yarn);
 
 var roger = new Cat("Roger");
 /**
- * Roger is sneaky and steals the ball from Amelia.
+ * Amelia shares her toy with Roger.
  */
-roger.steals = amelia.playWithBall;
-console.log(roger.steals());
+roger.toy = amelia.toy;
+roger.play();
 
 /**
- * Roger is sneaky and steals the ball from Josephine.
+ * Frank shares his toy with Roger.
  */
-roger.steals = josephine.excitedToPlayWith;
-console.log(roger.steals(yarn));
+roger.toy = frank.toy;
+roger.play();
